07e6742263ad416727dfbfae533c2d0b882b5bbf
Rose Luu <luuhongnhung10@gmail.com>
2021-09-20 18:47:02 -0500

    updated html, css

---
+++ script.js
@@ -0,0 +1,35 @@
+// What does HTML stand for?
+// Hypper Text Preprocessor
+// Hyper Text Markup Language
+// Hyper Text Multiple Language
+// Hyper Tool Multi Language
+//answer: 2
+
+// Commonly ued data types DO NOT include:
+// strings 
+// boolean 
+// alerts 
+// numbers 
+// answer: 3
+
+// The condition in an if/else statement is enclosed within _____.
+// quotes
+// curly brackets
+// parentheses
+// square brackets 
+// answer: 3
+
+// Arrays in JavaScript can be used to store ____.
+// numbers and strings 
+// other arrays 
+// booleans
+// at of the above 
+// answer: 4
+
+// A very useful tool used during development and debugging for printing content to the debugger is:
+// JavaScript
+// terminal/bash
+// for loops
+// console.log
+// answer: 4
+
